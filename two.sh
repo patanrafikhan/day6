0eed9066e68bff21c1f96ad573be654ee965f590
patanrafikhan <iamkhan342@gmail.com>
2023-01-09 14:07:45 +0530

    Added : Two^n Program

---
+++ two.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+read -p "Enter the number:" num
+p=1
+for ((i=2;i<=$num;i++))
+do
+        result=$(awk "BEGIN {printf  ${p}+ 1/${i} } ")
+        p=$(awk "BEGIN {printf  ${result}} ")
+done
+echo $p
